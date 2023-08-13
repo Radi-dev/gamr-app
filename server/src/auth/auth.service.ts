01ea469ba9f55079da8f0f3e1f89651833cc8e22
Radi <evaradi18@gmail.com>
2023-08-13 03:07:38 +0100

    validation

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,14 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
-  // constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
   signin() {
     return 'login';
   }
-  signup(dto: AuthDto) {
-    return { signuppy: dto };
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+    try {
+      const user = await this.prisma.user.create({
+        data: { email: dto.email, passHash: hash },
+      });
+      delete user.passHash;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+        throw error;
+      }
+    }
   }
 }
